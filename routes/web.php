39aab4a79f1dceaf8dc03eaf0ac55a9fc7ddf3fc
smemonislam6 <smemonislam6@gmail.com>
2024-08-18 01:54:44 +0600

    User Model add fillable Property.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'index'])->name('user.index');
+Route::post('/', [UserController::class, 'store'])->name('user.store');
